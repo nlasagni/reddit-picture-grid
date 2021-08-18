a2f35c08b5932d1125c53d800cf7da9eb792d6eb
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-08-18 17:52:59 +0200

    Adds RedditService to search posts of subreddit

---
+++ app/src/main/java/com/nlasagni/redditpicgrid/api/RedditService.kt
@@ -0,0 +1,67 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Nicola Lasagni
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.nlasagni.redditpicgrid.api
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.nlasagni.redditpicgrid.data.ListingRoot
+import com.nlasagni.redditpicgrid.data.Post
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+/**
+ * Used to connect to the Reddit API to fetch posts of subreddit.
+ *
+ * Created by Nicola Lasagni on 18/08/2021.
+ */
+interface RedditService {
+
+    @GET("r/{keyword}/top.json")
+    suspend fun searchSubredditPosts(@Path("keyword") keyword: String): ListingRoot?
+
+    companion object {
+        private const val BASE_URL = "https://www.reddit.com/"
+
+        fun create(): RedditService {
+            val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(RedditService::class.java)
+        }
+
+    }
+
+}
