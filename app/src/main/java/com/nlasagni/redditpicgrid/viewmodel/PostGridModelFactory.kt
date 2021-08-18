5bbb86ec4aa43a169c2d0e361cde86bf65e3750e
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-08-18 21:37:25 +0200

    Adds a factory that can convert data model into ui model

---
+++ app/src/main/java/com/nlasagni/redditpicgrid/viewmodel/PostGridModelFactory.kt
@@ -0,0 +1,54 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Nicola Lasagni
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.nlasagni.redditpicgrid.viewmodel
+
+import android.content.Context
+import com.nlasagni.redditpicgrid.R
+import com.nlasagni.redditpicgrid.data.ListingRoot
+import com.nlasagni.redditpicgrid.data.PostListMapper
+import com.nlasagni.redditpicgrid.viewmodel.model.PostGrid
+import com.nlasagni.redditpicgrid.viewmodel.model.PostGridItem
+import dagger.hilt.android.qualifiers.ActivityContext
+import javax.inject.Inject
+
+/**
+ * Created by Nicola Lasagni on 18/08/2021.
+ */
+class PostGridModelFactory @Inject constructor(
+    @ActivityContext private val context: Context,
+    private val mapper: PostListMapper) {
+
+    fun createModel(listingRoot: ListingRoot?): PostGrid {
+        val posts = mapper.mapToListOfPosts(listingRoot)?.map {
+            PostGridItem(
+                id = it.id,
+                title = it.title,
+                thumbnailUrl = it.thumbnail
+            )
+        } ?: emptyList()
+        return PostGrid(posts, context.resources.getString(R.string.no_post_match_search))
+    }
+
+}
