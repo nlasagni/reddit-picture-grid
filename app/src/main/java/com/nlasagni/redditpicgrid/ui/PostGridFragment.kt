3bef9296a73f9f438fd128d461bbb3229f66d8e8
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-08-18 22:47:50 +0200

    Adds TODO to empty fragment method

--- app/src/main/java/com/nlasagni/redditpicgrid/ui/PostGridFragment.kt
+++ app/src/main/java/com/nlasagni/redditpicgrid/ui/PostGridFragment.kt
@@ -30,7 +30,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import com.nlasagni.redditpicgrid.R
 import com.nlasagni.redditpicgrid.viewmodel.PostGridViewModel
@@ -63,7 +62,7 @@ class PostGridFragment : Fragment() {
     }
 
     private fun renderViewModel(postGrid: PostGrid) {
-
+        TODO("Not implemented")
     }
 
     private fun search(keyword: String) {
