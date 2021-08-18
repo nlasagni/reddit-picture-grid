682f1c904b6cd5f45501dc6340208823360db781
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-08-18 21:38:44 +0200

    Adds ViewModel related to post-grid

---
+++ app/src/main/java/com/nlasagni/redditpicgrid/viewmodel/PostGridViewModel.kt
@@ -0,0 +1,53 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Nicola Lasagni
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.nlasagni.redditpicgrid.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nlasagni.redditpicgrid.data.ListingRootDataSource
+import com.nlasagni.redditpicgrid.viewmodel.model.PostGrid
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+/**
+ * Created by Nicola Lasagni on 18/08/2021.
+ */
+@HiltViewModel
+class PostGridViewModel @Inject constructor(
+    private val dataSource: ListingRootDataSource,
+    private val viewModelFactory: PostGridModelFactory
+) : ViewModel() {
+
+    private val postGridLiveData = MutableLiveData<PostGrid>()
+
+    fun search(keyword: String) {
+        viewModelScope.launch {
+            postGridLiveData.value = viewModelFactory.createModel(dataSource.searchPosts(keyword))
+        }
+    }
+
+}
