32e40468d8edb5f73a8abaafd0d1dd386eab543f
Nicola Lasagni <nicola.lasagni@gmail.com>
2021-08-19 13:23:33 +0200

    Adds RemotePostImageManager to manage post image retrieval

---
+++ app/src/main/java/com/nlasagni/redditpicgrid/data/remote/RemotePostImageManager.kt
@@ -0,0 +1,57 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021 Nicola Lasagni
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.nlasagni.redditpicgrid.data
+
+import com.nlasagni.redditpicgrid.data.remote.ImageCheckStrategy
+import com.nlasagni.redditpicgrid.data.remote.Post
+import com.nlasagni.redditpicgrid.data.remote.RetrievePostImageUrlStrategy
+
+/**
+ * Created by Nicola Lasagni on 19/08/2021.
+ */
+class PostImageManager : ImageCheckStrategy, RetrievePostImageUrlStrategy {
+
+    private val imageTypeSuffixSet = setOf(".jpg", ".jpeg", ".png")
+
+    override fun isImage(post: Post): Boolean {
+        return endsWithImageSuffix(post.url) ||
+                endsWithImageSuffix(post.getFirstImage()?.url)
+    }
+
+    override fun retrieveThumbnailUrl(post: Post): String? {
+        return post.thumbnail
+    }
+
+    override fun retrieveImageUrl(post: Post): String {
+        // Here we assume that a post has at least one image url
+        val url = post.url!!
+        val imageUrl = post.getFirstImage()!!.url!!
+        return if (url.isNotEmpty()) url else imageUrl
+    }
+
+    private fun endsWithImageSuffix(string: String?): Boolean {
+        return imageTypeSuffixSet.any { string?.endsWith(it) ?: false }
+    }
+}
